67d5b0e385194077391b76d2b4c877f680df5131
Summer <xiaqiang@yunnex.com>
2018-05-29 22:03:57 +0800

    init

---
+++ src/test/java/org/xplatform/springcloudpractise/SpringCloudPractiseApplicationTests.java
@@ -0,0 +1,16 @@
+package org.xplatform.springcloudpractise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudPractiseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
